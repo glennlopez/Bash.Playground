786b48e1a0f65bf1c80eeb632cc71e08b476af51
glennlopez <glennlopez@gmail.com>
2015-09-17 20:51:13 -0700

    update

--- ver_test.sh
+++ ver_test.sh
@@ -5,28 +5,35 @@
 ##########################
 
 function script_update(){
-	# local script
-	awk '{ if ($1 ~ /#version/) print local $3}' ver_test.sh > tmp
-	loc_ver=$(<tmp)
-	mv ver_test.sh old.ver_test.sh
+	# check network connection
+	wget --spider --quiet https://raw.githubusercontent.com/glennlopez/qdGit/core/version
+	if [ "$?" == 0 ]; then
 
-	# remote script
-	wget --quiet https://raw.githubusercontent.com/glennlopez/Bash.Playground/master/ver_test.sh
-	awk '{ if ($1 ~ /#version/) print local $3}' ver_test.sh > tmp
-	rem_ver=$(<tmp)
+		# local script
+		awk '{ if ($1 ~ /#version/) print local $3}' ver_test.sh > tmp
+		loc_ver=$(<tmp)
+		mv ver_test.sh old.ver_test.sh
 
-	# compare versions for update
-	if [[ $loc_ver < $rem_ver ]]; then
-		rm -f old.ver_test.sh
-		echo 'Script was updated.'
+		# remote script
+		wget --quiet https://raw.githubusercontent.com/glennlopez/Bash.Playground/master/ver_test.sh
+		awk '{ if ($1 ~ /#version/) print local $3}' ver_test.sh > tmp
+		rem_ver=$(<tmp)
+
+		# compare versions for update
+		if [[ $loc_ver < $rem_ver ]]; then
+			rm -f old.ver_test.sh
+			echo 'Script was updated.'
+		else
+			echo 'Local version is up to date.'
+			rm -f ver_test.sh
+			mv old.ver_test.sh ver_test.sh
+		fi
+
+		chmod +x ver_test.sh
+		rm -f tmp
 	else
-		echo 'Local version is up to date.'
-		rm -f ver_test.sh
-		mv old.ver_test.sh ver_test.sh
+		echo 'Skipping update: Cannot establish connection to remote git repository.'
 	fi
-
-	chmod +x ver_test.sh
-	rm -f tmp
 }
 
 script_update
