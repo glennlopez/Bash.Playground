69c4f3856c5454fd6ee672eb22a1088308db975a
glennlopez <glennlopez@gmail.com>
2015-09-17 20:42:28 -0700

    update

--- ver_test.sh
+++ ver_test.sh
@@ -3,19 +3,18 @@
 #version = 0.8
 #debug = 0
 ##########################
+gitURL=$https://raw.githubusercontent.com/glennlopez/Bash.Playground/master/ver_test.sh
 
 function script_update(){
 	# local script
 	awk '{ if ($1 ~ /#version/) print local $3}' ver_test.sh > tmp
 	loc_ver=$(<tmp)
 	mv ver_test.sh old.ver_test.sh
-	echo $loc_ver #<--debug
 
 	# remote script
-	wget --quiet https://raw.githubusercontent.com/glennlopez/Bash.Playground/master/ver_test.sh
+	wget --quiet $gitURL
 	awk '{ if ($1 ~ /#version/) print local $3}' ver_test.sh > tmp
 	rem_ver=$(<tmp)
-	echo $rem_ver #<--debug
 
 	# compare versions for update
 	if [[ $loc_ver < $rem_ver ]]; then
@@ -25,8 +24,10 @@ function script_update(){
 		echo 'Local version is up to date.'
 		rm -f ver_test.sh
 		mv old.ver_test.sh ver_test.sh
-		chmod +x ver_test.sh
 	fi
+
+	chmod +x ver_test.sh
+	rm -f tmp
 }
 
 script_update
